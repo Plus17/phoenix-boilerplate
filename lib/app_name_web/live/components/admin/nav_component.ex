5ca4ef707f30343306f6d160df92eee69eb68e11
Sergio <me@sergiovp.dev>
2022-11-14 20:48:06 -0600

    feat: adds admin nav component

---
+++ lib/app_name_web/live/components/admin/nav_component.ex
@@ -0,0 +1,21 @@
+defmodule AppNameWeb.Components.Admin.NavComponent do
+  @moduledoc """
+  Component for Admin Nav links
+  """
+  use AppNameWeb, :component
+
+  def nav(assigns) do
+    assigns.socket |> IO.inspect(label: "ASSIGNS", printable_limit: :infinity, limit: :infinity)
+    ~H"""
+    <div class="flex-1 px-2 mx-2">Admin options</div>
+    <div class="flex-none hidden lg:block">
+      <ul class="menu menu-horizontal">
+        <!-- Navbar menu content here -->
+        <li>
+          <.link navigate={Routes.admin_live_path(@socket, AppNameWeb.Admin.UserLive)} class="underline" replace={true}>Users</.link>
+        </li>
+      </ul>
+    </div>
+    """
+  end
+end
