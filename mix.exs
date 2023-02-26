764f5df170c699d3a656ee19739701d0f9659f9a
Sergio <me@sergiovp.dev>
2023-02-25 18:20:06 -0600

    chore: update mix aliases

--- mix.exs
+++ mix.exs
@@ -86,17 +86,17 @@ defmodule AppName.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup", "npm.install"],
+      setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      "npm.install": ["cmd npm install --prefix ./assets"],
       test: [
         "ecto.create --quiet",
         "ecto.migrate --quiet",
         "test --warnings-as-errors"
       ],
-      ci: ["compile", "format --check-formatted", "credo --strict", "cmd MIX_ENV=test mix test"],
-      "assets.deploy": ["esbuild default --minify", "phx.digest"]
+      "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
+      "assets.build": ["tailwind default", "esbuild default"],
+      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
     ]
   end
 end
